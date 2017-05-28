acd45faddb626a097ee9347877fc8b0d223584fa
Vojtech Vondra <vojtavondra@gmail.com>
2017-05-28 20:34:45 +0200

    Set up tests in Travis

---
+++ setup-travis.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -o errexit
+
+KONG_VERSION=$1
+
+echo "Installing Kong version: $KONG_VERSION"
+
+# Installing other dependencies
+sudo apt-get update
+sudo apt-get install -y git curl make pkg-config unzip libpcre3-dev apt-transport-https
+
+# Assign permissions to "vagrant" user
+sudo chown -R vagrant /usr/local
+
+
+####################
+# Install Postgres #
+####################
+
+# Create PG user and database
+psql -U postgres <<EOF
+\x
+CREATE USER kong;
+CREATE DATABASE kong OWNER kong;
+CREATE DATABASE kong_tests OWNER kong;
+EOF
+
+################
+# Install Kong #
+################
+echo Fetching and installing Kong...
+wget -q -O kong.deb https://github.com/Mashape/kong/releases/download/$KONG_VERSION/kong-$KONG_VERSION.trusty_all.deb
+sudo apt-get update
+sudo apt-get install -y netcat openssl libpcre3 dnsmasq procps perl
+sudo dpkg -i kong.deb
+rm kong.deb
+
+# Adjust PATH
+export PATH=$PATH:/usr/local/bin:/usr/local/openresty/bin
+
+# Prepare path to lua libraries
+ln -sfn /usr/local /home/vagrant/.luarocks
+
+# Set higher ulimit
+sudo bash -c 'echo "fs.file-max = 65536" >> /etc/sysctl.conf'
+sudo sysctl -p
+sudo bash -c "cat >> /etc/security/limits.conf" << EOL
+* soft     nproc          65535
+* hard     nproc          65535
+* soft     nofile         65535
+* hard     nofile         65535
+EOL
+
+
+#############
+# Finish... #
+#############
+
+# Adjust PATH for future ssh
+echo "export PATH=\$PATH:/usr/local/bin:/usr/local/openresty/bin" >> /home/vagrant/.bashrc
+
+# Adjust LUA_PATH to find the plugin dev setup
+echo "export LUA_PATH=\"/kong-plugin/?.lua;/kong-plugin/?/init.lua;;\"" >> /home/vagrant/.bashrc
+
+# Assign permissions to "vagrant" user
+sudo chown -R vagrant /usr/local
+
+echo "Successfully Installed Kong version: $KONG_VERSION"
